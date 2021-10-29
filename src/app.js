3d75f3deaa097b3b2e4d4eacf6cdd815dd3d103e
Ilya Sobolev <illenium.toy@gmail.com>
2021-10-30 02:37:24 +0300

    feature: add request and error loggers

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const { errors } = require('celebrate');
 
 const { handleErrors } = require('./middlewares/handleErrors');
 const { routes } = require('./routes');
+const { requestLogger, errorLogger } = require('./middlewares/loggers');
 
 const {
   PORT = 3000,
@@ -19,10 +20,12 @@ mongoose.connect(DB_CONNECT, {
   useNewUrlParser: true,
 });
 
+app.use(requestLogger);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+app.use(errorLogger);
 app.use(errors());
 app.use(handleErrors);
 
---
+++ src/middlewares/loggers.js
@@ -0,0 +1,21 @@
+const winston = require('winston');
+const expressWinson = require('express-winston');
+
+const requestLogger = expressWinson.logger({
+  transports: [
+    new winston.transports.File({ filename: '../logs/request.log' }),
+  ],
+  format: winston.format.json(),
+});
+
+const errorLogger = expressWinson.errorLogger({
+  transports: [
+    new winston.transports.File({ filename: '../logs/error.log' }),
+  ],
+  format: winston.format.json(),
+});
+
+module.exports = {
+  requestLogger,
+  errorLogger,
+};
