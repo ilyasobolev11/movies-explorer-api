c7c1afc6e2f1eaed301855900faea40295fc8cef
Ilya Sobolev <illenium.toy@gmail.com>
2021-10-31 02:59:13 +0300

    bugfix: fix logs directory path

--- src/middlewares/loggers.js
+++ src/middlewares/loggers.js
@@ -3,14 +3,14 @@ const expressWinson = require('express-winston');
 
 const requestLogger = expressWinson.logger({
   transports: [
-    new winston.transports.File({ filename: '../logs/request.log' }),
+    new winston.transports.File({ filename: './logs/request.log' }),
   ],
   format: winston.format.json(),
 });
 
 const errorLogger = expressWinson.errorLogger({
   transports: [
-    new winston.transports.File({ filename: '../logs/error.log' }),
+    new winston.transports.File({ filename: './logs/error.log' }),
   ],
   format: winston.format.json(),
 });
