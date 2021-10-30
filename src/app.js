2f1edd79c950b7f329daa6ebcc5b9364f5552f8a
Ilya Sobolev <illenium.toy@gmail.com>
2021-10-30 03:01:54 +0300

    feature: add rateLimiter, cors, helmet middlewares

--- src/app.js
+++ src/app.js
@@ -3,16 +3,35 @@ require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const cors = require('cors');
 const { errors } = require('celebrate');
 
 const { handleErrors } = require('./middlewares/handleErrors');
 const { routes } = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/loggers');
+const { rateLimiter } = require('./middlewares/rateLimiter');
 
 const {
   PORT = 3000,
   DB_CONNECT = 'mongodb://localhost:27017/moviesdb',
 } = process.env;
+
+const corsWhiteList = [
+  'http://localhost:3000',
+  'https://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (corsWhiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
 
 const app = express();
 
@@ -21,6 +40,9 @@ mongoose.connect(DB_CONNECT, {
 });
 
 app.use(requestLogger);
+app.use(rateLimiter);
+app.use(cors(corsOptions));
+app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
