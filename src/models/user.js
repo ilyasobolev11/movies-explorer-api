4b02a4e35b9d1fb95590408a0301d40054262fe6
Ilya Sobolev <illenium.toy@gmail.com>
2021-10-30 16:04:40 +0300

    bugfix: fix findUserByCredentials statics method of userSchema

--- src/models/user.js
+++ src/models/user.js
@@ -27,14 +27,14 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.static.findUserByCredentials = async function ({ email, password }) {
+userSchema.statics.findUserByCredentials = async function ({ email, password }) {
   const user = await this
     .findOne({ email })
     .select('+password')
     .orFail(new AuthError('Неправильные почта или пароль'));
 
   const matched = await bcrypt.compare(password, user.password);
-  if (matched) {
+  if (!matched) {
     throw new AuthError('Неправильные почта или пароль');
   }
   return user;
